f961e35416b2aabb01c7d3a3b27eb71d2a3f19d0
Marija Ratnikova <122369597+MariaRatnikova@users.noreply.github.com>
2025-04-26 23:52:17 +0200

    Update Verlag.java

--- java_projekt/Verlag.java
+++ java_projekt/Verlag.java
@@ -1,4 +1,17 @@
 public class Verlag {
-	private String _name__String;
-	private String _adresse__String;
+    private String name;
+
+    public Verlag(String name) {
+        this.name = name;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Verlag{name='" + name + "'}";
+    }
 }
