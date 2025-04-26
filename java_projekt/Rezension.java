27b737263710d2345570e763bc1f1bf983143779
Marija Ratnikova <122369597+MariaRatnikova@users.noreply.github.com>
2025-04-26 23:51:35 +0200

    Update Rezension.java

--- java_projekt/Rezension.java
+++ java_projekt/Rezension.java
@@ -1,5 +1,29 @@
+import java.util.Date;
+
 public class Rezension {
-	private String _bewertung__int;
-	private String _kommentar__String;
-	private String _datum__Date;
+    private int bewertung;
+    private String kommentar;
+    private Date datum;
+
+    public Rezension(int bewertung, String kommentar, Date datum) {
+        this.bewertung = bewertung;
+        this.kommentar = kommentar;
+        this.datum = datum;
+    }
+
+    public int getBewertung() {
+        return bewertung;
+    }
+
+    public String getKommentar() {
+        return kommentar;
+    }
+
+    public Date getDatum() {
+        return datum;
+    }
+    @Override
+    public String toString() {
+        return "Rezension{bewertung=" + bewertung + ", kommentar='" + kommentar + "', datum=" + datum + "}";
+    }
 }
