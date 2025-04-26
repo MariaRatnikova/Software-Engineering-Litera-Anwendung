65beda589839826c40793fadd2ba36fe411eaa9c
Marija Ratnikova <122369597+MariaRatnikova@users.noreply.github.com>
2025-04-26 23:51:16 +0200

    Update Genre.java

--- java_projekt/Genre.java
+++ java_projekt/Genre.java
@@ -1,4 +1,30 @@
 public class Genre {
-	private String _name__String;
-	private String _beschreibung__String;
+    private String name;
+    private String beschreibung;
+
+    public Genre(String name, String beschreibung) {
+        this.name = name;
+        this.beschreibung = beschreibung;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBeschreibung() {
+        return beschreibung;
+    }
+
+    public void setBeschreibung(String beschreibung) {
+        this.beschreibung = beschreibung;
+    }
+
+    @Override
+    public String toString() {
+        return name + ": " + beschreibung;
+    }
 }
